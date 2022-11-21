392f0e4296e8aef6fb9c222325c9a723a499d6c4
Ilan <smilan273@gmail.com>
2022-11-21 21:26:44 +0200

    add makefile & basicClassification

--- basicClassification.c
+++ basicClassification.c
@@ -1,3 +1,37 @@
-//
-// Created by 97253 on 15/11/2022.
-//
+#include "NumClass.h"
+#include <stdio.h>
+#include <math.h>
+
+
+#define True 1;
+#define False 0;
+
+// help function for isStrong function
+int factorial(int a) {
+    if (a == 1 || a == 0)
+        return 1;
+    return factorial(a-1) * a;
+}
+
+int isPrime(int a) {
+    int i;
+    for(i = a-1 ; i > 1 ; i--){
+        if(a % i == 0)
+            return False;
+    }
+    return True;
+}
+int isStrong(int x) {
+    int sum, a = 0, b = x;
+    while (b != 0) {
+        a = b % 10;
+        sum += factorial(a);
+        b = b / 10;
+    }
+    if(sum == x)
+        return True
+    return False;
+}
+
+
+
