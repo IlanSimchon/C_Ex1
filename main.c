a451c2c172e1e1585df3ef32fda6a272f3ca362a
Ilan <smilan273@gmail.com>
2022-11-22 22:45:47 +0200

    fix spaces

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(){
     printf("The Armstrong numbers are:");
     for (int i = start; i < end; i++) {
         if(isArmstrong(i))
-            printf("%d " ,i);
+            printf(" %d" ,i);
     }
     printf("\n");
 
@@ -18,7 +18,7 @@ int main(){
     printf("The Palindrome numbers:");
     for (int i = start; i < end; i++) {
         if(isPalindrome(i))
-            printf("%d " ,i);
+            printf(" %d" ,i);
     }
     printf("\n");
 
@@ -26,7 +26,7 @@ int main(){
     printf("The Prime numbers:");
     for (int i = start; i < end; i++) {
         if(isPrime(i))
-            printf("%d " ,i);
+            printf(" %d" ,i);
     }
     printf("\n");
 
@@ -34,7 +34,7 @@ int main(){
     printf("The Strong numbers are:");
     for (int i = start; i < end; i++) {
         if(isStrong(i))
-            printf("%d " ,i);
+            printf(" %d" ,i);
     }
     printf("\n");
 
